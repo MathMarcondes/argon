a627a18daa871d42f81b5f8afd05da8b0df5c48e
MathMarcondes <matheus.marcondes@outlook.com>
2025-03-10 18:10:05 -0300

    backgroundColor -

---
+++ app/Home/index.tsx
@@ -0,0 +1,74 @@
+import { Text, StyleSheet, View, TouchableOpacity, Button } from "react-native";
+
+
+
+import {LinearGradient} from 'expo-linear-gradient';
+
+export default function Home() {
+  return (
+    <LinearGradient style={styles.container}
+        colors={["#281483", "#8F6ED5", "#D782D9"]}
+        
+        start={{x: 1, y: 0}}
+        end={{x: 0, y: 1}}
+        >
+      <Text style={{
+        color: '#fff', 
+        fontWeight: 'bold', 
+        alignItems: 'center',
+        paddingStart: '35%',
+        marginTop: '25%',
+        alignContent: 'center',
+        fontSize: 45
+
+      }}>argon</Text>
+
+
+      <Text style={{
+        color: '#fff', 
+        fontWeight: 'bold', 
+        alignItems: 'center',
+        paddingStart: '10%',
+        marginTop: '45%',
+        alignContent: 'center',
+        fontSize: 57
+
+      }}>Design System</Text>
+
+      <Text style={{
+        color: '#fff',
+        paddingStart: '10%',
+        fontSize: 16,
+        marginTop: 20
+
+      }}>Fully coded React Native components.</Text>
+
+      <TouchableOpacity activeOpacity={0.9}
+      style={{
+        backgroundColor: '#fff',
+        marginTop: '40%',
+        marginStart: '10%',
+        marginEnd: '10%',
+        paddingTop: 8,
+        paddingBottom: 8,
+        borderRadius: 5}}>
+          <Text style={{
+            fontWeight: 'bold',
+            marginStart: '38%'
+            }}>Get Started</Text>
+      </TouchableOpacity>
+
+      
+
+      
+    </LinearGradient>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  }
+})
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import Home from './Home';
+
+
+export default function app() {
+ return (
+   
+    <Home/>
+    
+  );
+}
